f04120780af5da02ba4a75dc7ca5a9fbb12b79fd
jafewe <mwelle231@gmail.com>
2025-03-16 01:59:22 +0100

    change loading order and added colorscheme in init.lua

--- init.lua
+++ init.lua
@@ -1,14 +1,17 @@
+-- [[ vim settings ]]
+require 'vim-settings'
+
+-- [[ keybindings is use ]]
+require 'keybindings'
+
 -- [[ Install the 'lazy.nvim' package manager ]]
 require 'lazy-install'
 
 -- [[ Configure the lazy plugins to be isntalled ]]
 require 'lazy-plugins'
 
--- [[ vim settings ]]
-require 'vim-settings'
-
--- [[ keybindings is use ]]
-require 'keybindings'
+-- colorscheme
+vim.cmd.colorscheme 'catppuccin-mocha'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
