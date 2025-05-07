1caf6ca16f8cb5abd8d613d319d423accc3d920e
jafewe <mwelle231@gmail.com>
2025-05-07 22:14:43 +0200

    start settings up snacks.nvim

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -168,6 +168,10 @@ return {
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       -- Some languages (like typescript) have entire language plugins that can be useful:
       --    https://github.com/pmizio/typescript-tools.nvim
+
+      -- html server
+      html = {},
+
       lua_ls = {
         -- cmd = { ... },
         -- filetypes = { ... },
@@ -182,7 +186,6 @@ return {
           },
         },
       },
-      html = {},
     }
 
     -- Ensure the servers and tools above are installed
@@ -196,12 +199,13 @@ return {
     -- `mason` had to be setup earlier: to configure its options see the
     -- `dependencies` table for `nvim-lspconfig` above.
     --
-    -- You can add other tools sere that you want Mason to install
+    -- You can add other tools here that you want Mason to install
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
     })
+
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
     require('mason-lspconfig').setup {
