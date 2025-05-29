60dc65e8c9719aa9361cd06cc162987b02bb459b
jafewe <mwelle231@gmail.com>
2025-05-29 20:50:58 +0200

    add lspkind and source it's icons in cmp

--- lua/kickstart/plugins/nvim-cmp.lua
+++ lua/kickstart/plugins/nvim-cmp.lua
@@ -55,8 +55,8 @@ return { -- Autocompletion
       -- <c-e>: Hide menu
       -- <c-k>: Toggle signature help
       --
-      -- See :h blink-cmp-config-keymap for defining your own keymap
       preset = 'enter',
+      -- See :h blink-cmp-config-keymap for defining your own keymap
       -- For more advanced Luasnip keymaps (e.g. selevtiong choice nodes, expansion) see:
       --      https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
     },
@@ -71,6 +71,44 @@ return { -- Autocompletion
       -- By default, you may press `<c-space>` to show the documentation.
       -- Optionally, set `auto_show = true` to show documentation after a delay.
       documentation = { auto_show = true, auto_show_delay_ms = 500 },
+      -- config from blink cmp site Recipes web-devicons + lspkind
+      menu = {
+        draw = {
+          components = {
+            kind_icon = {
+              text = function(ctx)
+                local icon = ctx.kind_icon
+                if vim.tbl_contains({ 'Path' }, ctx.source_name) then
+                  local dev_icon, _ = require('nvim-web-devicons').get_icon(ctx.label)
+                  if dev_icon then
+                    icon = dev_icon
+                  end
+                else
+                  icon = require('lspkind').symbolic(ctx.kind, {
+                    mode = 'symbol',
+                  })
+                end
+
+                return icon .. ctx.icon_gap
+              end,
+
+              -- Optionally, use the highlight groups from nvim-web-devicons
+              -- You can also add the same function for `kind.highlight` if you want to
+              -- keep the highlight groups in sync with the icons.
+              highlight = function(ctx)
+                local hl = ctx.kind_hl
+                if vim.tbl_contains({ 'Path' }, ctx.source_name) then
+                  local dev_icon, dev_hl = require('nvim-web-devicons').get_icon(ctx.label)
+                  if dev_icon then
+                    hl = dev_hl
+                  end
+                end
+                return hl
+              end,
+            },
+          },
+        },
+      },
     },
 
     sources = {
